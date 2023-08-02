7c6bd861caf05e389a777e4e39b55a714376ac64
Joyce <joycewachira718@gmail.com>
2023-08-02 20:47:49 +0300

    completed assignmet

---
+++ bb/src/Components/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import BotCollection from './Components/BotCollection';
+import YourBotArmy from './Components/YourBotArmy';
+
+const App = () => {
+  const [bots, setBots] = useState([]);
+  const [botArmy, setBotArmy] = useState([]);
+
+  useEffect(() => {
+    fetch('https://bot-data-yjzx.onrender.com/bots')
+      .then((response) => response.json())
+      .then((data) => setBots(data));
+  }, []);
+
+  const handleEnlistBot = (bot) => {
+    // Check if the bot is not already enlisted
+    if (!botArmy.some((enlistedBot) => enlistedBot.id === bot.id)) {
+      setBotArmy([...botArmy, bot]);
+    }
+  };
+
+  const handleReleaseBot = (botId) => {
+    // Remove the bot from the botArmy based on its id
+    setBotArmy(botArmy.filter((enlistedBot) => enlistedBot.id !== botId));
+  };
+
+  const handleDischargeBot = (botId) => {
+    // Remove the bot from the backend
+    fetch(`https://bot-data-yjzx.onrender.com/bots/${botId}`, {
+      method: 'DELETE',
+    }).then((response) => {
+      if (response.ok) {
+        // Remove the bot from the botArmy based on its id
+        setBotArmy(botArmy.filter((enlistedBot) => enlistedBot.id !== botId));
+      }
+    });
+  };
+
+  return (
+    <div>
+      <h1>Bot Army Enlistment</h1>
+      <BotCollection botArmy={botArmy} bots={bots} onEnlistBot={handleEnlistBot} />
+      <YourBotArmy
+        botArmy={botArmy}
+        onReleaseBot={handleReleaseBot}
+        onDischargeBot={handleDischargeBot}
+      />
+    </div>
+  );
+};
+
+export default App;
+
